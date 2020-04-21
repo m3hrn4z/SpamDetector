8e83cf885cbbd1e01fe06c125574e442c7230f5e
naghmeh <nagmhehshafiee@gmail.com>
2020-04-21 18:02:06 -0400

    training part completed, regex needs edition based on professors answer in discussion forum

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import os
 import re
-import email
 
 
 def list_directory_files(directory):
@@ -15,7 +14,6 @@ def extract_file_class_from_its_name(file_name):
 
 
 def preprocess_file_and_create_vocabulary(filename):
-    print(filename)
     f = open(filename, "rb")
     text = f.read()
     f.close()
@@ -23,9 +21,6 @@ def preprocess_file_and_create_vocabulary(filename):
     # find encoding source https: // stackoverflow.com / questions / 31019854 / typeerror - cant - use - a - string - pattern - on - a - bytes - like - object - in -re - findall
     text = text.decode('ISO-8859-1')
 
-    # regex source "https://stackoverflow.com/questions/6202549/word-tokenization-using-python-regular-expressions"
-    # words = re.findall("[A-Z\-\']{2,}(?![a-z])|[A-Z\-\'][a-z\-\']+(?=[A-Z])|[\'\w\-]+", lower_text)
-    # words = re.split("[a-zA-Z]+", lower_text)
     words = re.split('\[\^a-zA-Z\]', text)
     words_list = words[0].split()
 
@@ -35,9 +30,20 @@ def preprocess_file_and_create_vocabulary(filename):
 def create_frequency_table(corpus_folder):
     files_list = list_directory_files(corpus_folder)
     vocab_freq_dict = {}
+
+    total_document_count = 0
+    ham_document_count = 0
+    spam_document_count = 0
+
     for file_name in files_list:
         file_category = extract_file_class_from_its_name(file_name)
         list_of_vocabs_in_file = preprocess_file_and_create_vocabulary(file_name)
+
+        total_document_count += 1
+        if file_category == 'ham':
+            ham_document_count += 1
+        else:
+            spam_document_count += 1
 
         # word_count source https://towardsdatascience.com/very-simple-python-script-for-extracting-most-common-words-from-a-story-1e3570d0b9d0
         for vocabulary in list_of_vocabs_in_file:
@@ -53,29 +59,47 @@ def create_frequency_table(corpus_folder):
                 else:
                     vocab_freq_dict[vocabulary] = [0, 1]
 
-    return vocab_freq_dict
+        p_ham = ham_document_count / total_document_count
+        p_spam = spam_document_count / total_document_count
+
+    return vocab_freq_dict, p_ham, p_spam
 
 
 def compute_conditional_probability_with_smoothing(vocab_freq_dict, delta):
-
     total_ham_count = 0
     total_spam_count = 0
 
-    for vocab,freq in vocab_freq_dict.items():
+    for vocab, freq in vocab_freq_dict.items():
         total_ham_count += freq[0] + delta
         total_spam_count += freq[1] + delta
 
-    for vocab,freq in vocab_freq_dict.items():
-        ham_probability = (freq[0]+delta)/total_ham_count
-        spam_probability = (freq[1]+delta)/total_spam_count
-        freq.insert(1,ham_probability)
+    for vocab, freq in vocab_freq_dict.items():
+        ham_probability = (freq[0] + delta) / total_ham_count
+        spam_probability = (freq[1] + delta) / total_spam_count
+        freq.insert(1, ham_probability)
         freq.append(spam_probability)
     return vocab_freq_dict
 
 
+def generate_model_file(vocab_dict, model_file):
+    file1 = open(model_file, "w")
+    sorted_key_list = sorted(vocab_dict)
+    line_counter = 0
+    for vocab in sorted_key_list:
+        line_counter += 1
+        line_string = '%i  %s  %i  %f  %i  %f\n' % (line_counter, vocab, vocab_dict[vocab][0],
+                                                    vocab_dict[vocab][1], vocab_dict[vocab][2],
+                                                    vocab_dict[vocab][3])
+        file1.write(line_string)
+    file1.close()
+    return
+
+
 if __name__ == '__main__':
     delta = 0.5
-    vocab_freq_dict = create_frequency_table('train')
-    vocab_freq_probability_dict = compute_conditional_probability_with_smoothing(vocab_freq_dict,delta)
+    vocab_freq_dict, p_ham, p_spam = create_frequency_table('train')
+    vocab_freq_probability_dict = compute_conditional_probability_with_smoothing(vocab_freq_dict, delta)
+    generate_model_file(vocab_freq_probability_dict, 'model.txt')
 
-    print(vocab_freq_probability_dict)
+    print('ham probability = ', p_ham)
+    print('spma probability = ', p_spam)
